7363d5727aa4cd7b83ff679aee4d2ab0d199159f
daehoanshin <xbb123@naver.com>
2018-09-19 22:10:47 +0900

    URL로 입력된 값 사용하기

--- nodejs/web2-nodejs/main2.js
+++ nodejs/web2-nodejs/main2.js
@@ -5,7 +5,6 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    console.log(queryData);
     console.log(queryData.id);
     if(_url == '/'){
       _url = '/index.html';
